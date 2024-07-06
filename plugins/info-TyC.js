9c8673e40e78f0dc8db0bce0af5888b385433764
Jesusoo000 <159474846+Jesusoo000@users.noreply.github.com>
2024-07-06 10:50:04 -0300

    Actualizar info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -1,4 +1,4 @@
-///const handler = async (m, {conn}) => {
+./const handler = async (m, {conn}) => {
   m.reply(global.terminos);
 };
 handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
@@ -22,4 +22,4 @@ global.terminos = `𝑺𝑴𝑺 𝑫𝑬𝑳 𝑪𝑹𝑬𝑨𝑫𝑶𝑹✨
 ✫ Correo electronico: 
 theluffybot@gmail.com
 
-✰ ${global.packname}`;///
+✰ ${global.packname}`;./
