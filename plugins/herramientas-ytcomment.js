c969639226d92304223b1c2fae883845717d060a
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-07 17:00:28 -0300

    Actualizar herramientas-ytcomment.js

--- plugins/herramientas-ytcomment.js
+++ plugins/herramientas-ytcomment.js
@@ -1,10 +1,6 @@
 const handler = async (m, {conn, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.maker_ytcomment
-
-  if (!text) throw 'No Text';
+  
+  if (!text) throw 'Ingresa texto';
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
