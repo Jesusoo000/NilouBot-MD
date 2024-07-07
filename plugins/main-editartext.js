d5773e3c8ed857a965d2e21275709b62ec560357
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-07 16:24:14 -0300

    Actualizar main-editartext.js

--- plugins/main-editartext.js
+++ plugins/main-editartext.js
@@ -1,25 +1,23 @@
-const fs = require('fs')
+const { promises } = require('fs')
+const { join } = require('path')
 
 let handler = async (m, { text }) => {
     if (!text) return m.reply('Por favor, proporciona el nuevo texto.')
-    
-    // Leer el archivo del menú
-    const menuPath = join(__dirname, '../path/to/plugins/main-menu.js') 
+
+    const menuPath = join(__dirname, '../path/to/plugins/main-menu.js')
     let menuContent = await promises.readFile(menuPath, 'utf-8')
-    
-    
+
     const beforeTextRegex = /before: `[\s\S]*?`,/
     menuContent = menuContent.replace(beforeTextRegex, `before: \`${text.trim()}\`,`)
-    
-    // Guardar los cambios en el archivo
+
     await promises.writeFile(menuPath, menuContent, 'utf-8')
-    
+
     m.reply('El texto del menú se ha actualizado con éxito.')
 }
 
 handler.help = ['editartext <nuevo texto>']
 handler.tags = ['main']
-handler.command = ['editartex'] 
-handler.rowner = true
+handler.command = ['editartext']
+handler.owner = true
 
 export default handler
