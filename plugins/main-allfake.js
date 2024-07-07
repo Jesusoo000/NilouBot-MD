c1058492404cb7621c07bdbdd567f9a94d034e3b
Jesusoo000 <159474846+Jesusoo000@users.noreply.github.com>
2024-07-07 09:07:34 -0300

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -95,7 +95,7 @@ global.rcanal = {
 contextInfo: {
 isForwarded: true,
 forwardedNewsletterMessageInfo: {
-newsletterJid: "120363317976257829@newsletter",
+newsletterJid: "120363204070691596@newsletter",
 serverMessageId: 100,
 newsletterName: 'Nilou Bot - Channel 🌸',
 },
