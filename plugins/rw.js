c790e8dbead25af53d6177bbe5a47c6783681b7b
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-11 04:39:17 -0300

    Actualizar rw.js

--- plugins/rw.js
+++ plugins/rw.js
@@ -1,10 +1,55 @@
-import axios from "axios"
-let handler = async (m, {command, conn, usedPrefix}) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-${command}.json`)).data  
-let haha = await res[Math.floor(res.length * Math.random())]    
-//conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
-conn.sendButton(m.chat, `_${command}_`.trim(), wm, haha, [['SIGUIENTE IMÁGEN ', `${usedPrefix + command}`]], null, null, m)    
+import axios from 'axios'
+
+global.db = global.db || { data: { users: {} } }
+
+let handler = async function (m, { conn, command, usedPrefix }) {
+  if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = { claimedImages: [], tempAnime: null }
+
+  if (command === 'rw') {
+    try {
+      let res = await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-${command}.json`)
+      let data = res.data
+      let randomIndex = Math.floor(Math.random() * data.length)
+      let imageUrl = data[randomIndex]
+
+      global.db.data.users[m.sender].tempAnime = imageUrl
+
+      await conn.sendMessage(m.chat, {
+        image: { url: imageUrl },
+      }, { quoted: m })
+
+      await m.reply('Se ha enviado una imagen de anime. Responde con "claim" o "c" para reclamarla.')
+    } catch (error) {
+      console.error(error)
+      await m.reply('Hubo un error al obtener la imagen de anime. Por favor, intenta nuevamente más tarde.')
+    }
+  }
+
+  if (command === 'claim' || command === 'c') {
+    let user = global.db.data.users[m.sender]
+    if (!user.tempAnime) throw 'No hay imagen de anime para reclamar. Usa el comando `rw` primero.'
+    
+    user.claimedImages.push(user.tempAnime)
+    delete user.tempAnime
+
+    await m.reply('Has reclamado la imagen de anime.')
+  }
+
+  if (command === 'listrw') {
+    let user = global.db.data.users[m.sender]
+    if (!user.claimedImages.length) throw 'No has reclamado ninguna imagen de anime.'
+
+    let message = 'Imágenes de anime reclamadas:\n\n'
+    user.claimedImages.forEach((anime, index) => {
+      message += `${index + 1}. ${anime}\n\n`
+    })
+
+    await conn.sendMessage(m.chat, message.trim(), 'textMessage', { quoted: m })
+  }
 }
-handler.command = handler.help = ['rw', 'claim','c','listrw']
+
+handler.help = ['rw', 'claim', 'c', 'listrw']
 handler.tags = ['anime']
+handler.command = ['rw', 'claim', 'c', 'listrw']
+
 export default handler
