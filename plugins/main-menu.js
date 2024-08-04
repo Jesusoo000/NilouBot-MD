6f30023cf4a45c42deb0506319c87437fee45063
EnzioOfc <159474846+Jesusoo000@users.noreply.github.com>
2024-08-03 22:52:55 -0300

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -58,7 +58,7 @@ const defaultMenu = {
 
 \t*L I S T  -  C O M M A N D S* 
 `.trimStart(),
-  header: '*╭━━━━━━ • ✿「 %category 」✿ • ━━━━━━╮*',
+  header: '*╭•°• ✾ •°•「 %category 」•°• ✾ •°•╮*',
   body: '*│* ᪒❥ %cmd\n',
   footer: '*╰━━━━━━》❈《 ━━━━━╯*\n',
   after: '',
