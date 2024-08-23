93efbc8df9c6c0ed779209a65c4619b8fb335cb1
EnzioOfc <159474846+Jesusoo000@users.noreply.github.com>
2024-08-23 20:04:59 -0300

    Actualizar youtube-audio.js

--- plugins/youtube-audio.js
+++ plugins/youtube-audio.js
@@ -16,7 +16,7 @@ const yt = await fg.yta(urls[0], q)
 let { title, dl_url, size } = yt 
 
 if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
-conn.reply(m.chat, `🕒 *Descargando Su Audio Espere...*`, m, {
+conn.reply(m.chat, `🕒 *Descargando El Audio*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
